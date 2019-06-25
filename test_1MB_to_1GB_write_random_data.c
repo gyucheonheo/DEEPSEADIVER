5c8d0557c4ca689961d2fc388c24692e6718e049
Gyucheon Heo <gyucheon.heo@gmail.com>
2019-06-25 19:57:11 +0000

    Add random string generator function

--- test_1MB_to_1GB_write_random_data.c
+++ test_1MB_to_1GB_write_random_data.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *generate_rand_string(size_t);
 
 int main(){
-  size_t size = 1024UL*1024UL*2UL;
-  int i;
-
-  for(i = 1; i<=10; i++){
-    char *buf = (char *)malloc(sizeof(char)*size*i);
-    FILE *fp = fopen("test_100MB_to_1GB_write.dat", "w+");    
-    fwrite(buf, size*i, 1, fp);
-    fclose(fp);
-    free(buf);
-  }
+  FILE *fp = fopen("test.dat", "w");
+  fwrite(generate_rand_string(1024*1024), (1024*1024)+1, 1, fp);
+  fclose(fp);
 }
+
+char *generate_rand_string(size_t length){
+  static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+  char *rand_str;
+  
+  rand_str = malloc(length+1);
+  int i;
+  if ( rand_str ){
+    for(i=0; i < length; i++){
+      int key = rand() % (int)(sizeof(charset) - 1);
+      rand_str[i] = charset[key];
+    }
+    rand_str[length] = '\0';
+  }
+  return rand_str;
+}
+
+
