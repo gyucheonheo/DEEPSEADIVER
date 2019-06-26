92191c11dacfd3ebf92218e8877b0ae16f3b47e9
Gyucheon Heo <gyucheon.heo@gmail.com>
2019-06-26 16:01:41 +0000

    Update the size due to the page table size

--- test_1MB_to_1GB_write_random_data.c
+++ test_1MB_to_1GB_write_random_data.c
@@ -5,17 +5,23 @@
 char *generate_rand_string(size_t);
 
 int main(){
-  FILE *fp = fopen("test.dat", "w");
-  fwrite(generate_rand_string(1024*1024), (1024*1024)+1, 1, fp);
-  fclose(fp);
+  size_t size = 1024UL*1024UL*1024UL;
+  int i;
+  char *rand_str;
+  for(i = 1; i <= 3; i++){
+    FILE *fp = fopen("test.dat", "w");
+    rand_str = generate_rand_string((size*i)-1); // -1 because of the page size (4K)
+    fwrite(rand_str, (size*i), 1, fp);
+    fclose(fp);
+    free(rand_str);
+  }
 }
 
 char *generate_rand_string(size_t length){
   static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
   char *rand_str;
-  
   rand_str = malloc(length+1);
-  int i;
+  size_t i;
   if ( rand_str ){
     for(i=0; i < length; i++){
       int key = rand() % (int)(sizeof(charset) - 1);
