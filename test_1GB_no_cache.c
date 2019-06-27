f4a8ca843d0b904168f39646d090c4c1a28284f9
Gyucheon Heo <gyucheon.heo@gmail.com>
2019-06-27 17:24:35 +0000

    Add test case; 1GB with random string

---
+++ test_1GB_no_cache.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "randstr.h"
+
+int main(){
+  size_t size = 1024UL*1024UL1*1024UL;
+  FILE *fp = fopen("test_1GB_no_cache.c", "w");
+  char *rand_str = generate_rand_string((size-1));
+  fwrite(rand_str, size, 1, fp);
+  fclose(fp);
+  free(rand_str);
+}
