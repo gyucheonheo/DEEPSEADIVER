47ec10bf925a2c15ac390928821a0bd14ddc50ff
gyucheonheo <gyucheon.heo@gmail.com>
2019-07-23 11:22:44 -0500

    Fix missing closing bracket
    None

--- rdtscp/rdtscp.h
+++ rdtscp/rdtscp.h
@@ -6,5 +6,5 @@ static __inline__ unsigned long long rdtscp(void)
 	unsigned hi, lo;
 	__asm__ __volatile__ ("rdtscp" : "=a"(lo), "=d"(hi));
 	return ( (unsigned long long)lo)|( ((unsigned long long)hi) << 32)
-
+}
 #endif
