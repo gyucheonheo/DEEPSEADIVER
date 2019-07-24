07ce8929feae46317655939475f0988d19cfa190
gyucheonheo <gyucheon.heo@gmail.com>
2019-07-23 18:34:25 -0700

    Improve RDTSCP for precision

--- rdtscp/rdtscp.c
+++ rdtscp/rdtscp.c
@@ -1,19 +1,35 @@
+#define __STDC_FORMAT_MACROS
+
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "rdtscp.h"
 
-unsigned long long write_t(from,to,n)
+uint64_t write_t(from,to,n)
      char *from;
      char *to;
      size_t n;
 {
 	int i;
-	unsigned long long start, end;
-	start = rdtscp();
+	uint64_t start, end;
+
+	unsigned hi_start, hi_end, lo_start, lo_end;
+	__asm__ __volatile__ ("CPUID\n\t"
+			      "RDTSC\n\t"
+			      "mov %%edx, %0\n\t"
+			      "mov %%eax, %1\n\t" : "=r" (hi_start), "=r" (lo_start)::
+			      "%rax", "%rbx", "%rcx", "%rdx");
+
 	for(i = 0; i < n; i++){
 		from[i] = to[i];
 	}
-	end = rdtscp();
+
+	__asm__ __volatile ("RDTSCP\n\t"
+			    "mov %%edx, %0\n\t"
+			    "mov %%eax, %1\n\t"
+			    "CPUID\n\t": "=r" (hi_end), "=r" (lo_end)::
+			    "%rax", "%rbx", "%rcx", "%rdx");
+	start = (uint64_t)lo_start | (((uint64_t)hi_start) << 32);
+	end = (uint64_t)lo_end | (((uint64_t)hi_end) << 32);
 	return (end-start);
 }
 
@@ -24,7 +40,7 @@ int main(argc,argv)
 	size_t n = atoi(argv[1]);
  	char *from = (char *)malloc(sizeof(char)*n);
 	char *to = (char *)malloc(sizeof(char)*n);
-	printf("%llu cycles\n", write_t(from, to, n));
+	printf("%"PRIu64"\n", write_t(from, to, n));
 	return 0;
 }
 
